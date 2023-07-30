407243cfc8f206349ada4915a32fd01eed9ec7bb
devxprite <devxprite@gmail.com>
2023-07-30 13:53:49 +0530

    Add  Audience Poll

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -3,8 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { motion } from "framer-motion"
 import { pauseTimer } from "../../store/slices/quizSlice";
 import { useEffect } from "react";
+import { BiLoaderCircle } from 'react-icons/bi'
+import { RiLoader5Fill } from 'react-icons/ri'
 
-const Dialog = ({ children, onClose, isOpen = true, title }) => {
+const Dialog = ({ children, onClose, isOpen = true, title, loader = false }) => {
+    const [showLoader, setShowLoader] = useState(loader);
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -28,28 +31,47 @@ const Dialog = ({ children, onClose, isOpen = true, title }) => {
     if (!isOpen) return null;
 
     return (
-        <div
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.2 }}
             onClick={handleOuterClick}
             className="fixed top-0 left-0 z-10 w-screen h-screen bg-black/40 backdrop-blur-sm flex items-center justify-center"
         >
 
-            <motion.div
-                initial={{ scale: 0, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{type: "spring", duration: 0.3, stiffness: 260, damping: 20 }}
-                className="bg-violet-950 p-4 md:p-8 rounded-lg border-2 border-violet-500 w-[90vw] max-w-xl shadow-lg overflow-hidden"
-            >
+            {
+                showLoader ? (
+                    <motion.div
+                        key={'load'}
+                        initial={{ rotate: 0 }}
+                        animate={{ rotate: 360 }}
+                        transition={{ duration: 1, repeat: 2 }}
+                        onAnimationComplete={() => setShowLoader(false)}
+                    >
+                        <RiLoader5Fill className="text-5xl md:text-7xl text-white" />
+                    </motion.div>
+                ) : (
+                    <motion.div
+                        key={'dialog'}
+                        initial={{ scale: 0, opacity: 0 }}
+                        animate={{ scale: 1, opacity: 1 }}
+                        transition={{ type: "spring", duration: 1.2, bounce: 0.35, delay: 0.2 }}
+                        className="bg-violet-950 p-4 md:p-8 rounded-lg border-2 border-violet-500 w-[90vw] max-w-xl shadow-lg overflow-hidden"
+                        exit={{ scale: 0, opacity: 0 }}
+                    >
+                        {title && <h2 className="text-xl md:text-2xl mb-4 font-bold text-center">{title}</h2>}
+                        {children}
+                        {/* <button
+                            onClick={close}
+                            className=" bg-violet-700 px-4 py-1 shadow-md border-violet-500 border rounded-md mt-4 md:mt-8 ml-auto mr-0 block">
+                            Close
+                        </button> */}
+                    </motion.div>
+                )
 
 
-                {title && <h2 className="text-xl md:text-2xl mb-4 font-bold text-center">{title}</h2>}
-                {children}
-                <button
-                    onClick={close}
-                    className=" bg-violet-700 px-4 py-1 shadow-md border-violet-500 border rounded-md mt-4 md:mt-8 ml-auto mr-0 block">
-                    Close
-                </button>
-            </motion.div>
-        </div >
+            }
+        </motion.div >
     );
 }
 
--- src/components/Lifelines.jsx
+++ src/components/Lifelines.jsx
@@ -6,6 +6,7 @@ import { useLifeLine } from '../../store/slices/quizSlice';
 import phoneFriend from '../../utils/phoneFriend';
 import Dialog from './Dialog';
 import { useState } from 'react';
+import { motion } from "framer-motion"
 
 
 const iconsObj = {
@@ -42,6 +43,27 @@ const LifeLines = ({ answer, prefix }) => {
                 </>
             )
         }
+
+        if (lifeline == 'ask_the_audience') {
+            return (
+                <div className='grid grid-cols-4 justify-items-center gap-y-2'>
+                    {['A', 'B', 'C', 'D'].map((e, i) => (
+                        <motion.div
+                            key={e}
+                            animate={{
+                                scaleY: [0.25, 1, 0.5, (e == prefix ? 1 : Math.abs((Math.random() - 0.5) + 0.25))],
+                            }}
+                            transition={{ duration: 1.5, repeat: 3, repeatType: 'mirror', delay: i * 0.2, ease: 'linear' }}
+                            className='w-6 md:w-12 h-40 md:h-60 bg-gradient-to-b from-amber-300 to-amber-500  rounded origin-bottom' >
+                        </motion.div>
+                    ))}
+                    {['A', 'B', 'C', 'D'].map(e => ( <p className='font-bold md:text-xl'>{e}</p> ))
+                    }
+
+                </div>
+            )
+        }
+
     }
 
 
@@ -49,9 +71,7 @@ const LifeLines = ({ answer, prefix }) => {
         dispatch(useLifeLine(lifeline));
         setSelected(lifeline)
 
-        if (lifeline == 'phone_a_friend') {
-            setIsDialogOpen(true);
-        }
+        if (lifeline == 'phone_a_friend' || lifeline == 'ask_the_audience') setIsDialogOpen(true)
     }
 
     return (
@@ -67,7 +87,7 @@ const LifeLines = ({ answer, prefix }) => {
                     </div>
                 ))}
             </div>
-            {isDialogOpen && <Dialog onClose={() => setIsDialogOpen(false)}>
+            {isDialogOpen && <Dialog onClose={() => setIsDialogOpen(false)} loader={false}>
                 {dialogContent(selected)}
             </Dialog>}
         </>
